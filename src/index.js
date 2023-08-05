73bd284f04566ed654a978d242670f82cff04b6c
Andres <andresm.espinosas15@gmail.com>
2023-08-05 17:10:09 -0500

    added final touches

--- src/index.js
+++ src/index.js
@@ -13,27 +13,22 @@ const showNotification = (message) => {
 };
 
 const getScores = async (gameId) => {
-  const response = await fetch(
-    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`,
-  );
+  const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`);
   const data = await response.json();
   return data.result;
 };
 
 const saveScore = async (gameId, userName, userScore) => {
-  const response = await fetch(
-    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        user: userName,
-        score: userScore,
-      }),
+  const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
     },
-  );
+    body: JSON.stringify({
+      user: userName,
+      score: userScore,
+    }),
+  });
 
   const data = await response.json();
   return data.result;
@@ -61,24 +56,22 @@ document.getElementById('refresh-btn').addEventListener('click', async () => {
   });
 });
 
-document
-  .getElementById('add-form')
-  .addEventListener('submit', async (event) => {
-    event.preventDefault();
+document.getElementById('add-form').addEventListener('submit', async (event) => {
+  event.preventDefault();
 
-    const nameInput = document.getElementById('name');
-    const scoreInput = document.getElementById('score');
+  const nameInput = document.getElementById('name');
+  const scoreInput = document.getElementById('score');
 
-    const userName = nameInput.value;
-    const userScore = parseInt(scoreInput.value, 10);
+  const userName = nameInput.value;
+  const userScore = parseInt(scoreInput.value, 10);
 
-    if (Number.isNaN(userScore) || userScore <= 0) {
-      showNotification('Please enter a valid score.');
-      return;
-    }
+  if (Number.isNaN(userScore) || userScore <= 0) {
+    showNotification('Please enter a valid score.');
+    return;
+  }
 
-    await saveScore(customGameId, userName, userScore);
-    nameInput.value = '';
-    scoreInput.value = '';
-    showNotification('Score submitted successfully!');
-  });
+  await saveScore(customGameId, userName, userScore);
+  nameInput.value = '';
+  scoreInput.value = '';
+  showNotification('Score submitted successfully!');
+});
